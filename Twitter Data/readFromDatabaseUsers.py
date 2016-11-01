59629900b07ee4e643a9638a89757c138ef74e07
Alexander Wheadon <alexanderwheadon@gmail.com>
2016-11-01 15:17:41 -0400

    Removed Useless loops

--- Twitter Data/readFromDatabaseUsers.py
+++ Twitter Data/readFromDatabaseUsers.py
@@ -5,80 +5,31 @@ import json
 import matplotlib.pyplot as plt
 import matplotlib.dates as dates
 import datetime as dt
-import itertools
 #requires mongopy and matplotlib to install use: pip install [matplotlib/mongopy]
-#Takes some time because of large number of tweets status is shown in console
-def checkExist(tweetUser, users):
-    x = 0
-    while(x<len(users)):
-        #print users[x] + ' ' + str(x) + ' ' + tweetUser
-        if users[x] == tweetUser:
-            return True
-        x+=1
-    return False
-
-
-def createPlot(test, name):
+def createPlot(title, col):
     days = [0,0,0,0,0,0,0,0,0,0,0]
-    test, test2 = itertools.tee(test)
-    users = []
-    for tweet in test:
-        currentUser = tweet['user']['screen_name']
-       
-        if(not(currentUser in users)):
-            users.append(currentUser)
-
-
-    for tweet2 in test2:
-        stringTweet = unicodedata.normalize('NFKD', tweet2['created_at']).encode('ascii', 'ignore')
-        if ("Oct 11" in stringTweet) and checkExist(tweet2['user']['screen_name'], users):
-            days[0] += 1
-            users.remove(tweet2['user']['screen_name'])
-        elif "Oct 12" in stringTweet and checkExist(tweet2['user']['screen_name'], users):
-            days[1] += 1
-            users.append("1")
-            users.remove(tweet2['user']['screen_name'])
-        elif "Oct 13" in stringTweet and checkExist(tweet2['user']['screen_name'], users):
-            days[2] += 1
-            users.remove(tweet2['user']['screen_name'])
-        elif "Oct 14" in stringTweet and checkExist(tweet2['user']['screen_name'], users):
-            days[3] += 1
-            users.remove(tweet2['user']['screen_name'])
-        elif "Oct 15" in stringTweet and checkExist(tweet2['user']['screen_name'], users):
-            days[4] += 1
-            users.remove(tweet2['user']['screen_name'])
-        elif "Oct 16" in stringTweet and checkExist(tweet2['user']['screen_name'], users):
-            days[5] += 1
-            users.remove(tweet2['user']['screen_name'])
-        elif "Oct 17" in stringTweet and checkExist(tweet2['user']['screen_name'], users):
-            days[6] += 1
-            users.remove(tweet2['user']['screen_name'])
-        elif "Oct 18" in stringTweet and checkExist(tweet2['user']['screen_name'], users):
-            days[7] += 1
-            users.remove(tweet2['user']['screen_name'])
-        elif "Oct 19" in stringTweet and checkExist(tweet2['user']['screen_name'], users):
-            days[8] += 1
-            users.remove(tweet2['user']['screen_name'])
-        elif "Oct 20" in stringTweet and checkExist(tweet2['user']['screen_name'], users):
-            days[9] += 1
-            users.remove(tweet2['user']['screen_name'])
-        elif "Oct 21" in stringTweet and checkExist(tweet2['user']['screen_name'], users):
-            days[10] += 1
-            users.remove(tweet2['user']['screen_name'])
-        else:
-            pass
-    plt.plot(date, days, label=name)
-    print name + ' Done'
+    
+    days[0] = len(col.distinct("user.screen_name", { "created_at" : {'$regex': "Oct 11"}}))
+    days[1] = len(col.distinct("user.screen_name", { "created_at" : {'$regex': "Oct 12"}}))
+    days[2] = len(col.distinct("user.screen_name", { "created_at" : {'$regex': "Oct 13"}}))
+    days[3] = len(col.distinct("user.screen_name", { "created_at" : {'$regex': "Oct 14"}}))
+    days[4] = len(col.distinct("user.screen_name", { "created_at" : {'$regex': "Oct 15"}}))
+    days[5] = len(col.distinct("user.screen_name", { "created_at" : {'$regex': "Oct 16"}}))
+    days[6] = len(col.distinct("user.screen_name", { "created_at" : {'$regex': "Oct 17"}}))
+    days[7] = len(col.distinct("user.screen_name", { "created_at" : {'$regex': "Oct 18"}}))
+    days[8] = len(col.distinct("user.screen_name", { "created_at" : {'$regex': "Oct 19"}}))
+    days[9] = len(col.distinct("user.screen_name", { "created_at" : {'$regex': "Oct 20"}}))
+    days[10] = len(col.distinct("user.screen_name", { "created_at" : {'$regex': "Oct 21"}}))
+    
+    plt.plot(date, days, label=title)
+    print "Created plot: " + title
     return days;
-
-
 
 firstDay = dt.date(2016,10,11)
 date = [firstDay + dt.timedelta(days=x) for x in range(0,11)]
 plt.gca().xaxis.set_major_formatter(dates.DateFormatter('%m/%d/%Y'))
 plt.gca().xaxis.set_major_locator(dates.DayLocator())
 sumDays = [0,0,0,0,0,0,0,0,0,0,0]
-
 
 con = MongoClient('localhost', 27017)
 db = con.streamData
@@ -91,46 +42,37 @@ appl = db.appl
 goog = db.goog
 yhoo = db.yhoo
 
-test = altcoin.find()
-CurrentSum = createPlot(test, "Altcoin")
-sumDays = [x+y for x,y in zip(sumDays,CurrentSum)] 
+CurrentSum = createPlot("Altcoin", altcoin)
+sumDays = [x+y for x,y in zip(sumDays,CurrentSum)]
 CurrentSum = None
-test = None
-test = bitcoin.find()
-CurrentSum = createPlot(test, "Bitcoin")
-sumDays = [x+y for x,y in zip(sumDays,CurrentSum)] 
+
+CurrentSum = createPlot("Bitcoin", bitcoin)
+sumDays = [x+y for x,y in zip(sumDays,CurrentSum)]
 CurrentSum = None
-test = None
-test = coindesk.find()
-CurrentSum = createPlot(test, "Coindesk")
-sumDays = [x+y for x,y in zip(sumDays,CurrentSum)] 
+
+CurrentSum = createPlot("Coindesk", coindesk)
+sumDays = [x+y for x,y in zip(sumDays,CurrentSum)]
 CurrentSum = None
-test = None
-test = cryptocurrency.find()
-CurrentSum = createPlot(test, "Cryptocurrency")
-sumDays = [x+y for x,y in zip(sumDays,CurrentSum)] 
+
+CurrentSum = createPlot("Cryptocurrency", cryptocurrency)
+sumDays = [x+y for x,y in zip(sumDays,CurrentSum)]
 CurrentSum = None
-test = None
-test = gold.find()
-CurrentSum = createPlot(test, "Gold")
-sumDays = [x+y for x,y in zip(sumDays,CurrentSum)] 
+
+CurrentSum = createPlot("Gold", gold)
+sumDays = [x+y for x,y in zip(sumDays,CurrentSum)]
 CurrentSum = None
-test = None
-test = appl.find()
-CurrentSum = createPlot(test, "appl")
-sumDays = [x+y for x,y in zip(sumDays,CurrentSum)] 
+
+CurrentSum = createPlot("Apple", appl)
+sumDays = [x+y for x,y in zip(sumDays,CurrentSum)]
 CurrentSum = None
-test = None
-test = goog.find()
-CurrentSum = createPlot(test, "goog")
-sumDays = [x+y for x,y in zip(sumDays,CurrentSum)] 
+
+CurrentSum = createPlot("Google", goog)
+sumDays = [x+y for x,y in zip(sumDays,CurrentSum)]
 CurrentSum = None
-test = None
-test = yhoo.find()
-CurrentSum = createPlot(test, "yhoo")
-sumDays = [x+y for x,y in zip(sumDays,CurrentSum)] 
+
+CurrentSum = createPlot("Yahoo", yhoo)
+sumDays = [x+y for x,y in zip(sumDays,CurrentSum)]
 CurrentSum = None
-test = None
 
 plt.gcf().autofmt_xdate()
 plt.xlabel('Date')
@@ -138,6 +80,7 @@ plt.ylabel('Number of Users')
 plt.title('Users')
 plt.legend()
 plt.show()
+
 plt.plot(date, sumDays, label="Number of Total Users")
 plt.gcf().autofmt_xdate()
 plt.xlabel('Date')
@@ -145,3 +88,4 @@ plt.ylabel('Number of Users')
 plt.title('Tweets')
 plt.legend()
 plt.show()
+
